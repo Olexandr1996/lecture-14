f1de66ba0da741c522d208ee49aeb9d093d52a5c
Olexandr1996 <longervs@gmail.com>
2021-11-21 13:24:56 +0200

    first com

---
+++ index.js
@@ -0,0 +1,28 @@
+//let arr = [1,2,3,4] 
+//let arr2 = [5,6,7,8] 
+//let sum = arr2.reduce(function(a,b){return a+b}) + arr.reduce(function(a,b){return a +b}) 
+//alert(sum)  
+
+
+
+//let arr = [15,20,19,18,17,28,29,36]
+//let filt = arr.filter(function myfunc(a){return a>=18}) 
+//alert(filt) 
+
+
+
+
+let arr = ["Apple", "Banana", "Kiwi"]; 
+let str = arr.join('! ')
+ 
+alert (str)
+
+
+
+
+
+
+
+
+
+
